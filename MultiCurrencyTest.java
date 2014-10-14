6f169cf25f1ff2c2b1f573560067a15f626d10d2
unknown <mattia.cappa@Satellite315.gruppoblueteam.lan>
2014-10-14 15:18:56 +0200

    Eliminati test non piu' utili

--- MultiCurrencyTest.java
+++ MultiCurrencyTest.java
@@ -16,16 +16,7 @@ public class MultiCurrencyTest {
 	public void testEquality() throws Exception {
 		assertTrue(Money.Dollar(5).equals(Money.Dollar(5)));
 		assertFalse(Money.Dollar(5).equals(Money.Dollar(6)));
-		assertTrue(Money.Franc(5).equals(Money.Franc(5)));
-		assertFalse(Money.Franc(5).equals(Money.Franc(6)));
 		assertFalse(Money.Franc(5).equals(Money.Dollar(5)));
-	}
-	
-	@Test
-	public void testFrancMultiplication() {
-		Money five = Money.Franc(5);
-		assertEquals(Money.Franc(10), five.times(2));
-		assertEquals(Money.Franc(15), five.times(3));
 	}
 	
 	@Test
