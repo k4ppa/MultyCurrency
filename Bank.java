e4d4020d9d8d8e2418a4349e10bb8963b89765ec
unknown <mattia.cappa@Satellite315.gruppoblueteam.lan>
2014-10-15 11:23:40 +0200

    Money non deve conoscere i rate, gestirli e' un lavoro di Bank

--- Bank.java
+++ Bank.java
@@ -1,12 +1,16 @@
 
 public class Bank {
-
+	
 	public Money reduce(Expression source, String to) {
-		return source.reduce(to);
+		return source.reduce(this, to);
 	}
 
 	public void addRate(String sourceCurrency, String toCurrency, int rate) {
 		
+	}
+	
+	public int rate(String currency, String to) {
+		return (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
 	}
 
 }
--- Expression.java
+++ Expression.java
@@ -1,4 +1,4 @@
 
 public interface Expression {
-	public Money reduce(String to);
+	public Money reduce(Bank bank, String to);
 }
